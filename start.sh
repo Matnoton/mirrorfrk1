8f8ed22b77306da1e25eee475fc3bb399afdb8ca
Matnoton <97377427+Matnoton@users.noreply.github.com>
2022-03-02 20:07:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-python3 update.py && python3 -m bot
+python3 update.py && gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
+# gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
